b4bd754e0f825d7a57815267d3a64e7592d56645
Drew Conly <drew.conly@cherwell.com>
2016-09-02 11:36:35 -0600

    init repo boiler plate

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+      <h1>Two Way Binding</h1>
+      `,
+    directives: []
+})
+export class AppComponent { }
